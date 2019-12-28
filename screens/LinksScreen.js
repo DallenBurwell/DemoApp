36d2a4e1a820c214552d3573321b985032c765c4
Dallen <dallen.burwell@gmail.com>
2019-12-28 00:11:11 -0700

    Changed to Demo Code

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -10,7 +10,7 @@ export default function LinksScreen() {
        * Go ahead and delete ExpoLinksView and replace it with your content;
        * we just wanted to provide you with some helpful links.
        */}
-      <DJLinksView />
+      <ExpoLinksView />
     </ScrollView>
   );
 }
